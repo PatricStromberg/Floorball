7d285d1ce7af3575dbb66cbaabdf4ba7c149e00c
patricstromberg <stromberg.patric@hotmail.com>
2018-01-09 14:38:00 +0100

    Initial check-in of module FloorballApp

---
+++ FloorballApp/FloorballApp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FloorballApp.Services;
+using FloorballApp.Views;
+using Xamarin.Forms;
+
+namespace FloorballApp
+{
+    public class App : Application
+    {
+        public static FloorballManager Manager { get; set; }
+
+        public App()
+        {
+            Manager = new FloorballManager(new ApiService());
+
+            MainPage = new NavigationPage(new MainPage()){ BarBackgroundColor = Color.FromRgb(255, 255, 51) };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ FloorballApp/Models/RoundNumber.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FloorballApp.Models
+{
+    public class RoundNumber : List<Round>
+    {
+        public string Title { get; set; }
+    }
+}
